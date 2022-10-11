2753ccd7c0beb8ec8e2f50a930649a4a9b11e716
Jacob Howe <howejacob1@gmail.com>
2022-10-11 14:16:50 -0400

    nothing-- halfway though it

--- UDP_Client-Reliable.py
+++ UDP_Client-Reliable.py
@@ -22,8 +22,10 @@ def is_opcode_valid(opcode):
     valid_opcodes = ["+", "-", "/", "*"]
     return opcode in valid_opcodes
 
-def send_str(conn, the_string):
-    conn.sendall(the_string.encode("utf-8"))
+def send_str(conn, the_string, addr_and_port):
+    conn.sendto(the_string.encode("utf-8"), addr_and_port)
+
+BUFFER_SIZE = 1024*1024
     
 opcode_descs = {620: "Invalid OC",
                 630: "Invalid operands"}
@@ -31,10 +33,9 @@ opcode_descs = {620: "Invalid OC",
 def main(filename):
     with open(filename) as file_handle:
         for example_line in file_handle.read().split("\n"):
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
-                conn.connect((HOST, PORT))
-                send_str(conn, example_line + "\n")
-                line = recv_until_newline(conn)
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as conn:
+                send_str(conn, example_line + "\n", (HOST, PORT))
+                line = conn.recvfrom(BUFFER_SIZE)
                 status_code, result = line.split()
                 try:
                     status_code = int(status_code)
--- UDP_Server-Reliable.py
+++ UDP_Server-Reliable.py
@@ -1,5 +1,6 @@
 #icky code repitition
 import socket
+import random
 
 HOST = "127.0.0.1"
 PORT = 5000
@@ -20,53 +21,54 @@ def is_opcode_valid(opcode):
     valid_opcodes = ["+", "-", "/", "*"]
     return opcode in valid_opcodes
 
-def send_str(conn, the_string):
-    conn.sendall(the_string.encode("utf-8"))
+def send_str(conn, the_string, addr):
+    conn.sendto(the_string.encode("utf-8"), addr)
 
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+buffer_size = 1024*1024
+
+def main(percent_chance, seed):
+    random.seed(seed)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         try:
             sock.bind((HOST, PORT))
             while True:
                 status_code = 200
                 result = -1
-
-                sock.listen()
+                bytes, addr = sock.recvfrom(buffer_size)
+                line = bytes.decode("utf-8")
+                if random.random() < percent_chance/100:
+                    print(f"{line} -> dropped")
                 
-                conn, addr = sock.accept()
                 def finish():
-                    send_str(conn, f"{status_code} {result}\n")
+                    send_str(conn, f"{status_code} {result}\n", addr)
                     print(f"{line} -> {status_code} {result}")
-                try:
-                    line = recv_until_newline(conn)
-                    opcode, arg_1_str, arg_2_str = line.split()
-                    if not is_opcode_valid(opcode):
-                        status_code = 620
+                
+                opcode, arg_1_str, arg_2_str = line.split()
+                if not is_opcode_valid(opcode):
+                    status_code = 620
+                else:
+                    try:
+                        arg_1 = int(arg_1_str)
+                        arg_2 = int(arg_2_str)
+                    except ValueError:
+                        status_code = 630
                     else:
-                        try:
-                            arg_1 = int(arg_1_str)
-                            arg_2 = int(arg_2_str)
-                        except ValueError:
+                        if 0 in [arg_1, arg_2]:
                             status_code = 630
                         else:
-                            if 0 in [arg_1, arg_2]:
-                                status_code = 630
+                            # now calculate what really happened
+                            if opcode == "+":
+                                result = arg_1 + arg_2
+                            elif opcode == "-":
+                                result = arg_1 - arg_2
+                            elif opcode == "/":
+                                result = arg_1 / arg_2
+                            elif opcode == "*":
+                                result = arg_1 * arg_2
                             else:
-                                # now calculate what really happened
-                                if opcode == "+":
-                                    result = arg_1 + arg_2
-                                elif opcode == "-":
-                                    result = arg_1 - arg_2
-                                elif opcode == "/":
-                                    result = arg_1 / arg_2
-                                elif opcode == "*":
-                                    result = arg_1 * arg_2
-                                else:
-                                    print("in trouble now")
-                    finish()
-                finally:
-                    conn.close()
+                                print("in trouble now")
+                finish()
         finally:
             sock.close()
 
@@ -83,4 +85,4 @@ def main():
                 #     # we have the line, now simply do an operation on it 
 
 if __name__=="__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
